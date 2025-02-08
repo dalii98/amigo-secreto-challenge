e05fffe0befacc6df6e0fc117e1dba5bf93a40ca
dalii98 <daliileon1998@gmail.com>
2025-02-08 11:40:09 -0500

    1. Crear un array para almacenar los nombres
    2. Implementa una función para agregar amigos
    
    3. Implementa una función para actualizar la lista de amigos
    
    4. Implementa una función para sortear los amigos
    
    5. Creación del archivo README.md

---
+++ README.md
@@ -0,0 +1,35 @@
+# Amigo Secreto Challenge  
+
+Este proyecto es un pequeño juego interactivo en JavaScript que permite a los usuarios agregar nombres a una lista y seleccionar aleatoriamente a un "amigo secreto".  
+
+## Características  
+✅ Agregar nombres a la lista de amigos.  
+✅ Mostrar la lista de amigos ingresados.  
+✅ Seleccionar aleatoriamente un amigo secreto.  
+
+## Instalación  
+No se requieren dependencias ni instalación. Solo descarga o clona este repositorio y abre el archivo `index.html` en un navegador.  
+
+```sh
+git clone https://github.com/dalii98/amigo-secreto-challenge.git
+
+## Uso
+1. Ingresa el nombre de un amigo en el campo de texto.
+2. Haz clic en "Agregar Amigo" para añadirlo a la lista.
+3. Cuando hayas agregado todos los amigos, haz clic en "Sortear Amigo Secreto".
+4. Se mostrará un amigo seleccionado al azar
+
+## Estructura del Proyecto
+📂 amigo-secreto-challenge
+│── 📄 index.html       # Interfaz principal
+│── 📄 styles.css       # Estilos CSS
+│── 📄 script.js        # Lógica del juego
+└── 📄 README.md        # Documentación del proyecto
+
+## Tecnologías
+* HTML
+* CSS
+* Javascript
+
+## Autor
+Creado por Magdali León.
--- app.js
+++ app.js
@@ -1,1 +1,37 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+const agregarAmigo = () => {
+    let input = document.getElementById("amigo");
+    let amigo = input.value.trim();
+
+    if (amigo !== "") {
+        amigos.push(amigo);
+        input.value = "";
+        mostrarAmigos();
+    } else {
+        alert("Por favor, inserte un nombre.");
+    }
+};
+
+const mostrarAmigos = () => {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    amigos.forEach((amigo, index) => {
+        let li = document.createElement("li");
+        li.textContent = `${index + 1}. ${amigo}`;
+        lista.appendChild(li);
+    });
+};
+
+const sortearAmigo = () => {
+    document.getElementById("listaAmigos").innerHTML='';
+    if (amigos.length > 0) {
+        let numeroAleatorio = Math.floor(Math.random() * amigos.length);
+        let amigoAleatorio = amigos[numeroAleatorio];
+        document.getElementById("resultado").innerHTML = `El amigo secreto sorteado es: <strong>${amigoAleatorio}</strong>`;
+    } else {
+        alert("Por favor, inserte amigos para poder sortear.");
+    }
+};
